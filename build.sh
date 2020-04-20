9efb82a01be9337fd30e8b4f2a0d535e3e156104
Gabriella Turek <turek@mpi-cbg.de>
2020-04-20 20:23:13 +0200

    Since I cannot clone into a non empty repo I have to work around it

--- build.sh
+++ build.sh
@@ -5,13 +5,22 @@ mv tmp/* .
 mv tmp/.* .
 rmdir tmp
 .travis/build.sh
-#cd ../clij-core
-#git clone --depth=50 --branch=master https://github.com/clij/clij-core
-#.travis/build.sh
-#cd ../clij-ops
-#git clone --depth=50 --branch=master https://github.com/clij/clij-ops
-#.travis/build.sh
-#cd ../clij-custom-convolution-plugin
-#git clone --depth=50 --branch=master https://github.com/clij/clij-custom-convolution-plugin
-#.travis/build.sh
-#cd ..
+cd ../clij-core
+git clone --depth=50 --branch=master https://github.com/clij/clij-core tmp
+mv tmp/* .
+mv tmp/.* .
+rmdir tmp
+.travis/build.sh
+cd ../clij-ops
+git clone --depth=50 --branch=master https://github.com/clij/clij-ops tmp
+mv tmp/* .
+mv tmp/.* .
+rmdir tmp
+.travis/build.sh
+cd ../clij-custom-convolution-plugin
+git clone --depth=50 --branch=master https://github.com/clij/clij-custom-convolution-plugin tmp
+mv tmp/* .
+mv tmp/.* .
+rmdir tmp
+.travis/build.sh
+cd ..
