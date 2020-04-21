0f277fdf32f17f6570ca54a5b5d7e495796de76d
Gabriella Turek <turek@mpi-cbg.de>
2020-04-21 11:44:43 +0200

    Need to build first

--- build.sh
+++ build.sh
@@ -3,29 +3,32 @@ git clone --depth=50 --branch=master https://github.com/clij/clij-parent-pom cli
 cd clij-parent-pom
 mvn install
 git checkout tags/1.5.8
-mvn install
 cd ../clij
 git clone --depth=50 --branch=master https://github.com/clij/clij tmp
 mv tmp/* .
 mv tmp/.* .
 rmdir tmp
+mvn -Dmaven.test.skip=true
 .travis/build.sh
 cd ../clij-core
 git clone --depth=50 --branch=master https://github.com/clij/clij-core tmp
 mv tmp/* .
 mv tmp/.* .
 rmdir tmp
+mvn -Dmaven.test.skip=true
 .travis/build.sh
 cd ../clij-ops
 git clone --depth=50 --branch=master https://github.com/clij/clij-ops tmp
 mv tmp/* .
 mv tmp/.* .
 rmdir tmp
+mvn -Dmaven.test.skip=true
 .travis/build.sh
 cd ../clij-custom-convolution-plugin
 git clone --depth=50 --branch=master https://github.com/clij/clij-custom-convolution-plugin tmp
 mv tmp/* .
 mv tmp/.* .
 rmdir tmp
+mvn -Dmaven.test.skip=true
 .travis/build.sh
 cd ..
