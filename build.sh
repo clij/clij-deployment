22681e9322fd3966e43a2d88a9236fedc97de980
Gabriella Turek <turek@mpi-cbg.de>
2020-04-23 14:19:40 +0200

    Testing keys

--- build.sh
+++ build.sh
@@ -1,45 +1,69 @@
 #!/bin/sh
-echo "== Locally install parent pom =="
+checkSuccess() {
+  if [[ $1 -ne 0 ]]; then
+    echo "==> FAILED: EXIT CODE $1"
+    exit 1
+  fi
+}
+installNewParentPom() {
+  echo "== Locally install parent pom v. $1 =="
+  cd ../clij-parent-pom
+  git checkout tags/$1
+  mvn install
+  cd ../$repo
+}
+##
+# Locally install latest parent pom
+##
+echo "== Locally install latest parent pom =="
 git clone --depth=50 --branch=master https://github.com/clij/clij-parent-pom clij-parent-pom
 cd clij-parent-pom
+parentpom_version=$(grep -ri "<version>" pom.xml | tail -n +2 | head -n 1 | sed -n -e 's/.*<version>\(.*\)<\/version>.*/\1/p')
+echo "== Latest parent pom version is $parentpom_version"
+# Keep track of parent pom versions, so we don't re-install existing ones
+ppom_array+=("$parentpom_version")
+echo "Array contains ${ppom_array[@]}"
 mvn install
-echo "== Locally install parent pom v. 1.5.8 =="
-git checkout tags/1.5.8
-mvn install
-cd ../clij
-git clone --depth=50 --branch=master https://github.com/clij/clij tmp
-mv tmp/* .
-mv tmp/.* .
-rmdir tmp
-echo "== clij build =="
-mvn -Dmaven.test.skip=true
-echo "== clij deploy =="
-.travis/build.sh
-cd ../clij-core
-git clone --depth=50 --branch=master https://github.com/clij/clij-core tmp
-mv tmp/* .
-mv tmp/.* .
-rmdir tmp
-echo "== clij-core build =="
-mvn -Dmaven.test.skip=true
-echo "== clij-core deploy =="
-.travis/build.sh
-cd ../clij-ops
-git clone --depth=50 --branch=master https://github.com/clij/clij-ops tmp
-mv tmp/* .
-mv tmp/.* .
-rmdir tmp
-echo "== clij-ops build =="
-mvn -Dmaven.test.skip=true
-echo "== clij-ops deploy =="
-.travis/build.sh
-cd ../clij-custom-convolution-plugin
-git clone --depth=50 --branch=master https://github.com/clij/clij-custom-convolution-plugin tmp
-mv tmp/* .
-mv tmp/.* .
-rmdir tmp
-echo "== clij-custom-convlution-plugin build =="
-mvn -Dmaven.test.skip=true
-echo "== clij-custom-convlution-plugin deploy =="
-.travis/build.sh
-cd ..
+##
+# Next we need to perform the buil in the following order
+#  clij-coremem
+#  clij-clearcl
+#  clij-core
+#  clij
+#  clij-ops
+#  clij-custom-convolution
+#  clij2
+#  clijx-weka
+#  clijx
+##
+repos="clij-coremem clij-clearcl clij-core clij clij-ops clij-custom-convolution clij2 clijx-weka clijx"
+repos="clij-coremem"
+#
+for repo in $repos
+do
+  cd ../$repo
+  echo "== NOW BUILDING $repo =="
+  git clone --depth=50 --branch=master https://github.com/clij/$repo tmp
+  mv tmp/* .
+  mv tmp/.* .
+  rmdir tmp
+  # Check what version of the parent pom is being used
+  ppom_version=$(grep -ri "<version>" pom.xml | head -n 1 | sed -n -e 's/.*<version>\(.*\)<\/version>.*/\1/p')
+  if [ "$ppom_version" != "$parentpom_version" ]; then
+    echo "== Uses parent pom version $ppom_version"
+    if [[ "${ppom_array[@]}" =~ "${ppom_version}" ]]; then
+      echo "Already installed";
+    else
+      echo "Need to install parent pom version $ppom_version"
+      installNewParentPom $ppom_version
+      ppom_array+=("$ppom_version" )
+      echo "Array contains ${ppom_array[@]}"
+    fi
+  fi
+  echo "== $repo build =="
+  mvn -Dmaven.test.skip=true install
+  checkSuccess $?
+  #echo "== $repo deploy =="
+  #.travis/build.sh
+  #checkSuccess $?
+done
--- clij/.travis/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-.travis/travis-build.sh $encrypted_8bc46b011822_key $encrypted_8bc46b011822_iv
